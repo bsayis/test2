4aa0b393ba476d90fecd149911b27d9e02ffe814
bsayis <batuhansayis@gmail.com>
2017-06-04 22:08:30 +0200

    dynamic content

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,16 @@
 
+
+
 from django.shortcuts import render
 
-# Create your views here.
-def index(request): 
-    # this is your new view
-    return render(request, 'index.html')
+def index(request):
+    # defining the variable
+    number = 6
+    thing = "Thing name"
+    # passing the variable to the view
+    return render(request, 'index.html', {
+        'number': number,
+        # don't forget to pass it in, and the last comma
+        'thing': thing,
+    })
 
