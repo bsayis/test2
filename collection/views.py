f3fa3f541eda194d85b776900d5e200157b260df
bsayis <batuhansayis@gmail.com>
2017-06-04 23:00:28 +0200

    edit page added

--- collection/views.py
+++ collection/views.py
@@ -2,6 +2,7 @@
 
 
 from django.shortcuts import render
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # the rewritten view!
@@ -21,3 +22,32 @@ def thing_detail(request, slug):
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
     })
+
+
+
+# add below your thing_detail view
+def edit_thing(request, slug):
+    # grab the object
+    thing = Thing.objects.get(slug=slug)
+    # set the form we're using
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply to
+        # the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
+    })
+
